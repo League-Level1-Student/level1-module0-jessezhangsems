bbe34dc6d07e19914f1f40a8828e36785efd246f
C2SDK <C2SDK@C47508>
2020-09-12 11:34:04 -0700

    houses.java

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,50 @@
 package _01_houses;
 
+import java.awt.AWTException;
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+
 public class Houses {
-	public void run() {
+	public void run()  {
+		Robot house=new Robot(); 
+		house.setX(0);
+		house.setY(550);
+		house.setSpeed(100);
+		house.setPenWidth(50);
+		house.penDown();
+		house.setPenColor(Color.green);
+		house.turn(90);
+		house.move(100);
+		house.setRandomPenColor();
+		house.turn(270);
+		house.move(500);
+		house.turn(90);
+		house.move(100);
+		house.turn(90);
+		house.move(500);
+		house.turn(270);
+		house.setPenColor(Color.green);
+		house.move(100);
+		house.setRandomPenColor();
+		house.turn(270);
+		house.move(300);
+		house.turn(90);
+		house.move(400);
+		house.turn(90);
+		house.move(300);
+		house.turn(270);
+		house.setPenColor(Color.green);
+		house.move(100);
+		house.setRandomPenColor();
+		house.turn(270);
+		house.move(500);
+		house.turn(90);
+		house.move(100);
+		house.turn(90);
+		house.move(500);
+		house.turn(270);
 		
 	}
 }
